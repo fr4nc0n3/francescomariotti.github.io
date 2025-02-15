32c22e01c7d8cccee541e70c352a9272442dd67e
Francesco Mariotti <francesco.mariotti01@gmail.com>
2025-02-15 22:17:31 +0100

    Aggiunto controllo password

---
+++ script.js
@@ -0,0 +1,36 @@
+const correctPassword = "francescopsw";
+const pswKey = "password"; //localstorage key
+
+const loginForm = document.getElementById("loginForm");
+const passwordField = document.getElementById("password");
+
+loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const password = passwordField.value;
+
+    if (isPasswordValid(password)) {
+        localStorage.setItem(pswKey, password);
+
+        unlock();
+    } else {
+        alert("Password errata. Riprova.");
+    }
+});
+
+window.onload = function () {
+    const storedPassword = localStorage.getItem(pswKey);
+
+    if (isPasswordValid(storedPassword)) {
+        unlock();
+    }
+};
+
+const unlock = () => {
+    document.getElementById("content").style.display = "block";
+    document.getElementById("loginForm").style.display = "none";
+};
+
+const isPasswordValid = (password) => {
+    return password === correctPassword;
+};
